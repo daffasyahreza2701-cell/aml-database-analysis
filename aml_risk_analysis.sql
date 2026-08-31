62544348ed7abe9ef8cf7777e8aabd37304641c6
Daffa Syahreza <daffasyahreza2701@gmail.com>
2026-08-31 16:19:00 +0700

    fix 1: analysis query test and fix, added ERD to repo

--- aml_risk_analysis.sql
+++ aml_risk_analysis.sql
@@ -1,25 +1,28 @@
-#Customers with disproportionately high payment amount relative to annual income
-\echo '=== Question 16: Customers with disproportionately high payment amount relative to annual income ==='
-SELECT c.customer_id, c.annual_income_usd, i.annual_premium_usd, 
-(i.annual_premium_usd / c.annual_income_usd) AS premium_to_income_ratio
-FROM customer c
-JOIN insurance_info i 
-ON c.customer_id = i.customer_id
-WHERE (i.annual_premium_usd / c.annual_income_usd) > 0.5;
+#Customers with disproportionately high insurance premiums relative to annual income
+\echo '=== Question 16: Customers with disproportionately high insurance premiums relative to annual income ==='
+SELECT
+e.customer_id, e.annual_income_usd, i.annual_premium_usd,
+ROUND(i.annual_premium_usd::numeric / e.annual_income_usd, 2) 
+AS premium_to_income_ratio
+FROM income_cust e
+JOIN insurance_info i
+ON e.customer_id = i.customer_id
+ORDER BY premium_to_income_ratio DESC
+LIMIT 20;
 
 #Customers with unusually high net worth relative to annual income and have no known inheritance
 \echo '=== Question 17: Customers with unusually high net worth relative to annual income and have no known inheritance  ==='
-SELECT c.customer_id, c.annual_income_usd, e.net_worth_usd,
-(e.net_worth_usd / c.annual_income_usd) AS networth_to_income_ratio
-FROM customer c
+SELECT c.customer_id, e.annual_income_usd, e.net_worth_usd,
+ROUND(e.net_worth_usd / e.annual_income_usd, 2) AS networth_to_income_ratio
+FROM customers c
 JOIN income_cust e
 ON c.customer_id = e.customer_id
-WHERE (e.net_worth_usd / c.annual_income_usd) > 10 AND e.inheritance_amount_local IS NULL;
+WHERE (e.net_worth_usd / e.annual_income_usd) > 10 AND e.inheritance_amount_local = 0;
 
 #Countries with the highest average customer wealth 
 \echo '=== Question 18: Countries with the highest average customer wealth  ==='
 SELECT c.country, AVG(e.net_worth_usd) AS average_net_worth
-FROM customer c
+FROM customers c
 JOIN income_cust e
 ON c.customer_id = e.customer_id   
 GROUP BY c.country
@@ -28,8 +31,8 @@ ORDER BY average_net_worth DESC;
 #Customers with unusually high income relative to their working years 
 \echo '=== Question 19: Customers with unusually high income relative to their working years ==='
 SELECT c.customer_id, c.years_working, i.annual_premium_usd,
-(i.annual_premium_usd / c.years_working) AS premium_to_working_years_ratio
-FROM customer c
+ROUND(i.annual_premium_usd / c.years_working, 2) AS premium_to_working_years_ratio
+FROM customers c
 JOIN insurance_info i
 ON c.customer_id = i.customer_id
 WHERE (i.annual_premium_usd / c.years_working) > 1000;
@@ -37,13 +40,13 @@ WHERE (i.annual_premium_usd / c.years_working) > 1000;
 #Customers that have both high premium-to-income and net-worth-to-income ratios
 \echo '=== Question 20:  Customers that have both high premium-to-income and net-worth-to-income ratios ==='
 SELECT
-c.customer_id, c.annual_income_usd, e.net_worth_usd, i.annual_premium_usd,
-(i.annual_premium_usd / c.annual_income_usd) AS premium_to_income_ratio,
-(e.net_worth_usd / c.annual_income_usd) AS networth_to_income_ratio
-FROM customer c
+c.customer_id, e.annual_income_usd, e.net_worth_usd, i.annual_premium_usd,
+ROUND(i.annual_premium_usd / e.annual_income_usd, 2) AS premium_to_income_ratio,
+ROUND(e.net_worth_usd / e.annual_income_usd, 2) AS networth_to_income_ratio
+FROM customers c
 JOIN income_cust e
 ON c.customer_id = e.customer_id
 JOIN insurance_info i
 ON c.customer_id = i.customer_id
-WHERE (i.annual_premium_usd / c.annual_income_usd) > 0.5
-AND (e.net_worth_usd / c.annual_income_usd) > 10;
+ORDER BY premium_to_income_ratio DESC
+LIMIT 20;
